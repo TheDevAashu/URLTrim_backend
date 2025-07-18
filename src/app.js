b908e88c775bc495b64741ec3d4b384ca0e79d14
Aashu Singh <aashusingh1308@gmail.com>
2025-07-18 21:38:32 +0530

    feat: Configure CORS

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import { config } from "dotenv";
 
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import urlRouter from "./routes/url.routes.js";
 
 config();
 const app = express();
-app.use( cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://url-trime-aashu-singhs-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/", urlRouter);
 
