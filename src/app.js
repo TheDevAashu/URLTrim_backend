8e833c5340af38939503f8592a07788f01aed50b
Aashu Singh <aashusingh1308@gmail.com>
2025-07-18 21:54:39 +0530

    fix: enforce CORS on Vercel

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-import { config } from "dotenv";
+// index.js  (or app.js)
+import express from 'express';
+import cors from 'cors';
+import urlRouter from './routes/url.routes.js';
 
-import express from "express";
-import cors from "cors";
-import urlRouter from "./routes/url.routes.js";
-
-config();
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://url-trime-aashu-singhs-projects.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://url-trime-aashu-singhs-projects.vercel.app'
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
-app.use("/", urlRouter);
+app.use('/', urlRouter);
 
 export default app;
